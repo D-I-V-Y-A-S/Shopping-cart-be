b13e291f52171429efe8609cbad656a3cc7d1dac
DIVYA S <divyaselvam1405@gmail.com>
2025-03-01 17:44:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const db = mongoose.connection
 db.on('error', (errorMessage) => console.log(errorMessage))
 db.once('open', () => console.log(`Connected successfully to database`))
 
+app.get('/',(req,res)=>{
+    res.status(200).send("Api connection successful!")
+})
+
 app.use(cors())
 app.use(express.json())
 
